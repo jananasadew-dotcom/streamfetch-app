fabbe3b41352d68bffeaa9bf48b8f043202bb383
jananasadew-dotcom <herathdayapala@gmail.com>
2026-06-20 20:31:18 +0530

    Add files via upload

--- api/server.js
+++ api/server.js
@@ -2,17 +2,16 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-
-app.use(express.static(path.join(__dirname)));
+// ප්‍රධාන ෆෝල්ඩර් එකේ (Root) තියෙන index.html එක නිවැරදිව පෙන්වීමට මඟ සැකසීම
+app.use(express.static(path.join(__dirname, '..')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, '..', 'index.html'));
 });
 
-// Vercel එකට වැඩ කරන්න මේ කොටස අත්‍යවශ්‍යයි
-module.exports = app; 
+module.exports = app;
 
+const PORT = process.env.PORT || 3000;
 if (process.env.NODE_ENV !== 'production') {
     app.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
